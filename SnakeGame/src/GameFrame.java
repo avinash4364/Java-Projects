f7b13edb06fcefc41bbba802aba4d8629eee1a75
Avinash Kumar Yadav <aky25199@gmail.com>
2022-10-13 00:52:42 +0530

    added new classes for game frame and its panel

---
+++ SnakeGame/src/GameFrame.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GameFrame extends JFrame {
+    public GameFrame() throws HeadlessException {
+
+    }
+}
---
+++ SnakeGame/src/GamePanel.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class GamePanel extends JPanel {
+    private static final int SCREEN_WIDTH = 1000;
+    private static final int SCREE_HEIGHT = 1000;
+    
+    public GamePanel() {
+
+    }
+}
