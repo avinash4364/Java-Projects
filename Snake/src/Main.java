c96c7dd137d8bce16641fefa5cafd51b8207795e
Avinash Kumar Yadav <aky25199@gmail.com>
2022-10-13 00:24:14 +0530

    Delete Snake directory

--- Snake/src/Main.java
+++
@@ -1,5 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        new GameFrame(); // as we are not going to use the game frame object we can just create its instance like this
-    }
-}
